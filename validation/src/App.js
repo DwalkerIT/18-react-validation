7f6861c910462111c325563e1c22d2303b2b8e35
DWalkerIT <DWalkerIT@gmail.com>
2021-02-11 11:18:02 -0500

    validation

---
+++ validation/src/App.js
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+//onEventName =
+
+function App() {
+  const [first, setFirst] = useState("");
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [website, setWebsite] = useState("");
+  // const [text, setText] = useState("");
+  // const [select, setSelect] = useState("");
+  function resetForm() {
+    setFirst("");
+    // setText("");
+    return;
+  }
+  function submitForm(e) {
+    if (password != confirmPassword) {
+      e.preventDefault();
+      alert(`Passwords Don't Match`);
+    } else {
+      alert("Form Was Submitted Successfully");
+    }
+  }
+  return (
+    <div className="container">
+      <h1>Profile Form-All Fields Required</h1>
+      <h2>{first}</h2>
+      <div className="signUp">
+        {/* <p>{text}</p> */}
+        {/* <h4>{select}</h4> */}
+
+        <button onClick={resetForm}>reset</button>
+        <form onSubmit={submitForm}>
+          <label>Name</label>
+          <input
+            required
+            placeholder="your name here"
+            type="text"
+            name="firstname"
+            // value={first}
+            onChange={(e) => setFirst(e.target.value)}
+          />
+          <label>Email</label>
+          <input
+            required
+            placeholder="your email here"
+            type="email"
+            name="E-mail"
+            // value={first}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>UserName</label>
+          <input
+            required
+            placeholder="your userName here"
+            type="text"
+            name="UserName"
+            // value={first}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <label>Password</label>
+          <input
+            required
+            placeholder="your password here"
+            type="password"
+            name="Password"
+            // value={first}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label>Confirm Password</label>
+          <input
+            required
+            placeholder="Confirm your password here"
+            type="password"
+            name="Confirm Password"
+            // value={first}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <label>Website</label>
+          <input
+            required
+            placeholder="http://your-Website.com"
+            type="url"
+            name="Website"
+            // value={first}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+          {/* <textarea
+          type="text"
+          name="firstname"
+          // value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <select name="" id="" onChange={(e) => setSelect(e.target.value)}>
+          <option value="">Make A Selection</option>
+          <option value="hello">hello</option>
+          <option value="bye">bye</option>
+        </select> */}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
